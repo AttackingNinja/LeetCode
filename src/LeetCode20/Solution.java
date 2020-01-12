b4bfa7326a8c7f1d743775143623d9797c55decd
Lee_Cooper <495203870@qq.com>
2020-01-12 09:55:25 +0800

    Stack01

--- src/LeetCode20/Solution.java
+++ src/LeetCode20/Solution.java
@@ -1,5 +1,32 @@
 package LeetCode20;
+import java.util.Stack;
 
 public class Solution {
-
+    static boolean isMatch(char a, char b){
+        if((a == '(' && b == ')') || (a == '[' && b == ']') || (a == '{' && b == '}'))
+            return true;
+        return false;
+    }
+    static boolean isValid(String s) {
+        if(s == null)
+            return true;
+        int l = s.length();
+        Stack<Character> stack = new Stack<Character>();
+        for (int i = 0; i < l; i++){
+            if(stack.empty() == false){
+                if(isMatch(stack.peek(), s.charAt(i)) == true)
+                    stack.pop();
+                else
+                    stack.push(s.charAt(i));
+            }
+            else
+                stack.push(s.charAt(i));
+        }
+        if(stack.empty() == true)
+            return true;
+        return false;
+    }
+    public static void main(String args[]){
+        System.out.println(isValid("{[]}"));
+    }
 }
