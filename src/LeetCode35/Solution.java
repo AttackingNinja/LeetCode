597dfeae6adb47ca1729f83601a5327bb4e209b2
Lee_Cooper <495203870@qq.com>
2020-01-26 09:51:35 +0800

    BinarySearch01

---
+++ src/LeetCode35/Solution.java
@@ -0,0 +1,23 @@
+package LeetCode35;
+
+public class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target)
+                return mid;
+            else if (nums[mid] < target)
+                left++;
+            else
+                right--;
+        }
+        return left;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.searchInsert(new int[]{1, 3, 5, 6}, 2));
+    }
+}
