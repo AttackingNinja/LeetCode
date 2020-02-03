2980ed5ec777715078b31c38de6a09ab0627fa16
Lee_Cooper <495203870@qq.com>
2020-02-03 10:48:27 +0800

    Divide&Conquer01

--- src/LeetCode169/Solution.java
+++ src/LeetCode169/Solution.java
@@ -1,7 +1,26 @@
 package LeetCode169;
 
 public class Solution {
-    public int majorityElement(int[] nums) {
 
+    int find(int i, int j, int[] nums) {
+        if (i == j)
+            return nums[i];
+        int leftMajor = find(i, (i + j) / 2, nums);
+        int rightMajor = find((i + j) / 2 + 1, j, nums);
+        if (leftMajor == rightMajor)
+            return leftMajor;
+        int leftMajorSum = 0;
+        int rightMajorSum = 0;
+        for (int k = i; k <= j; k++) {
+            if (nums[k] == leftMajor)
+                leftMajorSum++;
+            if (nums[k] == rightMajor)
+                rightMajorSum++;
+        }
+        return leftMajorSum > rightMajorSum ? leftMajor : rightMajor;
+    }
+
+    public int majorityElement(int[] nums) {
+        return find(0, nums.length - 1, nums);
     }
 }
