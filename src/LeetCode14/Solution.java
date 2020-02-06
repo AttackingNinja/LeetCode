b4ef1394511b7acacde3970dcd8e5d841481c557
Lee_Cooper <495203870@qq.com>
2020-02-06 10:15:20 +0800

    String01

---
+++ src/LeetCode14/Solution.java
@@ -0,0 +1,18 @@
+package LeetCode14;
+
+public class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs.length != 0) {
+            for (int i = 0; i < strs[0].length(); i++) {
+                char curChar = strs[0].charAt(i);
+                for (int j = 1; j < strs.length; j++) {
+                    if (i >= strs[j].length() || strs[j].charAt(i) != curChar) {
+                        return strs[0].substring(0, i);
+                    }
+                }
+            }
+            return strs[0];
+        }
+        return "";
+    }
+}
