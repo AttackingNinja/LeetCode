5429399f8cf39171f9ad7aa981731650574a4336
Lee_Cooper <495203870@qq.com>
2020-02-08 11:59:30 +0800

    String02

---
+++ src/LeetCode58/Solution.java
@@ -0,0 +1,12 @@
+package LeetCode58;
+
+public class Solution {
+    public int lengthOfLastWord(String s) {
+        s = s.trim();
+        int lastSpaceIndex = s.lastIndexOf(" ");
+        if (lastSpaceIndex == -1) {
+            return s.length();
+        }
+        return s.length() - 1 - lastSpaceIndex;
+    }
+}
