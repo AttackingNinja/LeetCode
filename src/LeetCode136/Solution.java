c4c20f00c44c20abefcd7b0fcd158b4e706781c3
Lee_Cooper <495203870@qq.com>
2020-02-16 12:48:58 +0800

    HashTable06

---
+++ src/LeetCode136/Solution.java
@@ -0,0 +1,18 @@
+package LeetCode136;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution {
+    public int singleNumber(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (!set.contains(nums[i])) {
+                set.add(nums[i]);
+            } else {
+                set.remove(nums[i]);
+            }
+        }
+        return set.iterator().next();
+    }
+}
