a8892d9e4a4a1fd487bc3c68252b4f2c4670f312
Lee_Cooper <495203870@qq.com>
2020-02-19 19:11:57 +0800

    LinkedList03

--- src/LeetCode21/Solution.java
+++ src/LeetCode21/Solution.java
@@ -11,31 +11,21 @@ class ListNode {
 
 public class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        if (l1 == null)
-            return l2;
-        if (l2 == null)
-            return l1;
-        ListNode head = l1.val <= l2.val ? l1 : l2;
+        ListNode preHead = new ListNode(-1);
+        ListNode cur = preHead;
         ListNode p = l1;
         ListNode q = l2;
         while (p != null && q != null) {
-            ListNode pBefore = null;
-            ListNode qBefore = null;
-            while (p != null && p.val <= q.val) {
-                pBefore = p;
+            if (p.val <= q.val) {
+                cur.next = p;
                 p = p.next;
-            }
-            if (pBefore != null) {
-                pBefore.next = q;
-            }
-            while (p != null && q != null && q.val <= p.val) {
-                qBefore = q;
+            } else {
+                cur.next = q;
                 q = q.next;
             }
-            if (qBefore != null) {
-                qBefore.next = p;
-            }
+            cur = cur.next;
         }
-        return head;
+        cur.next = p == null ? q : p;
+        return preHead.next;
     }
 }
