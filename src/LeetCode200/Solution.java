1d2a31133279744b6221276da1691e276f7ab5ca
Lee_Cooper <495203870@qq.com>
2020-02-21 16:13:16 +0800

    DFS02

--- src/LeetCode200/Solution.java
+++ src/LeetCode200/Solution.java
@@ -1,33 +1,35 @@
 package LeetCode200;
 
 
+import java.util.*;
+
 public class Solution {
     char[][] grid;
     boolean[][] discovered;
 
     void discover(int i, int j) {
         discovered[i][j] = true;
-        if (i - 1 >= 0 && grid[i - 1][j] == '1' && discovered[i - 1][j] == false)
+        if (i - 1 >= 0 && grid[i - 1][j] == '1' && !discovered[i - 1][j])
             discover(i - 1, j);
-        if (i + 1 < grid.length && grid[i + 1][j] == '1' && discovered[i + 1][j] == false)
+        if (i + 1 < grid.length && grid[i + 1][j] == '1' && !discovered[i + 1][j])
             discover(i + 1, j);
-        if (j - 1 >= 0 && grid[i][j - 1] == '1' && discovered[i][j - 1] == false)
+        if (j - 1 >= 0 && grid[i][j - 1] == '1' && !discovered[i][j - 1])
             discover(i, j - 1);
-        if (j + 1 < grid[0].length && grid[i][j + 1] == '1' && discovered[i][j + 1] == false)
+        if (j + 1 < grid[0].length && grid[i][j + 1] == '1' && !discovered[i][j + 1])
             discover(i, j + 1);
     }
 
-    public int numIslands(char[][] grid) {
+    public int numIslandsByDFS(char[][] grid) {
         int result = 0;
         if (grid.length != 0) {
             this.grid = grid;
             discovered = new boolean[grid.length][grid[0].length];
-            for (int m = 0; m < discovered.length; m++)
-                for (int n = 0; n < discovered[m].length; n++)
-                    discovered[m][n] = false;
+            for (boolean[] booleans : discovered) {
+                Arrays.fill(booleans, false);
+            }
             for (int i = 0; i < grid.length; i++)
                 for (int j = 0; j < grid[0].length; j++) {
-                    if (grid[i][j] == '1' && discovered[i][j] == false) {
+                    if (grid[i][j] == '1' && !discovered[i][j]) {
                         discover(i, j);
                         result++;
                     }
@@ -36,8 +38,54 @@ public class Solution {
         return result;
     }
 
+    public int numIslandsByBFS(char[][] grid) {
+        int result = 0;
+        if (grid.length != 0 && grid[0].length != 0) {
+            Queue<Integer> queue = new LinkedList<>();
+            boolean[][] discovered = new boolean[grid.length][grid[0].length];
+            for (boolean[] row : discovered) {
+                Arrays.fill(row, false);
+            }
+            for (int i = 0; i < grid.length; i++) {
+                for (int j = 0; j < grid[0].length; j++) {
+                    if (grid[i][j] == '1' && !discovered[i][j]) {
+                        result++;
+                        discovered[i][j] = true;
+                        queue.offer(i);
+                        queue.offer(j);
+                        while (!queue.isEmpty()) {
+                            int x = queue.poll();
+                            int y = queue.poll();
+                            if (x - 1 >= 0 && grid[x - 1][y] == '1' && !discovered[x - 1][y]) {
+                                discovered[x - 1][y] = true;
+                                queue.offer(x - 1);
+                                queue.offer(y);
+                            }
+                            if (x + 1 < grid.length && grid[x + 1][y] == '1' && !discovered[x + 1][y]) {
+                                discovered[x + 1][y] = true;
+                                queue.offer(x + 1);
+                                queue.offer(y);
+                            }
+                            if (y - 1 >= 0 && grid[x][y - 1] == '1' && !discovered[x][y - 1]) {
+                                discovered[x][y - 1] = true;
+                                queue.offer(x);
+                                queue.offer(y - 1);
+                            }
+                            if (y + 1 < grid[0].length && grid[x][y + 1] == '1' && !discovered[x][y + 1]) {
+                                discovered[x][y + 1] = true;
+                                queue.offer(x);
+                                queue.offer(y + 1);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.numIslands(new char[][]{{'1', '1', '1'}, {'0', '1', '0'}, {'1', '1', '1'}}));
+        System.out.println(solution.numIslandsByBFS(new char[][]{{'1', '1', '1'}, {'0', '1', '0'}, {'1', '1', '1'}}));
     }
 }
