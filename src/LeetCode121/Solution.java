f84d0bd250d422e42f464dbc03727383eb0ef93f
Lee_Cooper <495203870@qq.com>
2020-02-24 12:13:42 +0800

    DP01

--- src/LeetCode121/Solution.java
+++ src/LeetCode121/Solution.java
@@ -2,20 +2,17 @@ package LeetCode121;
 
 public class Solution {
     public int maxProfit(int[] prices) {
-        if (prices.length < 2) {
+        if (prices.length == 0) {
             return 0;
         } else {
             int[][][] dp = new int[prices.length][2][2];
-            for (int i = 0; i < prices.length; i++) {
-                for (int k = 1; k >= 1; k--) {
-                    if (i == 0) {
-                        dp[i][k][0] = 0;
-                        dp[i][k][1] = -prices[0];
-                    } else {
-                        dp[i][k][0] = Math.max(dp[i - 1][k][0], dp[i - 1][k][1] + prices[i]);
-                        dp[i][k][1] = Math.max(dp[i - 1][k][1], dp[i - 1][k - 1][0] - prices[i]);
-                    }
-                }
+            dp[0][0][0] = 0;
+            dp[0][0][1] = -prices[0];
+            dp[0][1][0] = 0;
+            for (int i = 1; i < prices.length; i++) {
+                dp[i][0][0] = dp[i - 1][0][0];
+                dp[i][0][1] = Math.max(dp[i - 1][0][1], dp[i - 1][0][0] - prices[i]);
+                dp[i][1][0] = Math.max(dp[i - 1][1][0], dp[i - 1][0][1] + prices[i]);
             }
             return dp[prices.length - 1][1][0];
         }
