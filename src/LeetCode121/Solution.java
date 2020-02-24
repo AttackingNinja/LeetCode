548dc743a044be8eb0cb5a2ec361729f813389be
Lee_Cooper <495203870@qq.com>
2020-02-24 12:17:18 +0800

    DP02

--- src/LeetCode121/Solution.java
+++ src/LeetCode121/Solution.java
@@ -17,11 +17,4 @@ public class Solution {
             return dp[prices.length - 1][1][0];
         }
     }
-
-    public static void main(String args[]) {
-        Solution solution = new Solution();
-        System.out.println(solution.maxProfit(new int[]{7, 1, 5, 3, 6, 4}));
-        System.out.println(solution.maxProfit(new int[]{7, 6, 4, 3, 1}));
-        System.out.println(solution.maxProfit(new int[]{1, 2}));
-    }
 }
