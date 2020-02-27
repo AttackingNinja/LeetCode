07dbfdf891c37d37bde959db7b6dd2213add4ccd
Lee_Cooper <495203870@qq.com>
2020-02-27 18:14:22 +0800

    HashTable03

--- src/LeetCode18/Solution.java
+++ src/LeetCode18/Solution.java
@@ -3,7 +3,7 @@ package LeetCode18;
 import java.util.*;
 
 public class Solution {
-    public List<List<Integer>> fourSum(int[] nums, int target) {
+    public List<List<Integer>> fourSumByHashTable(int[] nums, int target) {
         List<List<Integer>> results = new ArrayList<>();
         Map<Integer, List<List<Integer>>> map = new HashMap<>();
         for (int i = 0; i < nums.length - 1; i++)
@@ -11,7 +11,7 @@ public class Solution {
                 if (map.containsKey(target - nums[i] - nums[j])) {
                     List<List<Integer>> pairs = map.get(target - nums[i] - nums[j]);
                     for (List<Integer> pair : pairs) {
-                        if (pair.contains(i) == false && pair.contains(j) == false) {
+                        if (!pair.contains(i) && !pair.contains(j)) {
                             List<Integer> result = new ArrayList<>();
                             result.add(nums[i]);
                             result.add(nums[j]);
@@ -35,8 +35,56 @@ public class Solution {
         return results;
     }
 
+    public List<List<Integer>> fourSumByTwoPointers(int[] nums, int target) {
+        Arrays.sort(nums);
+        List<List<Integer>> results = new LinkedList<>();
+        for (int i = 0; i < nums.length - 3; i++) {
+            int curMin = nums[i] + nums[i + 1] + nums[i + 2] + nums[i + 3];
+            int curMax = nums[i] + nums[nums.length - 3] + nums[nums.length - 2] + nums[nums.length - 1];
+            if (curMin > target) {
+                return results;
+            }
+            if (curMax < target) {
+                continue;
+            }
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            for (int j = i + 1; j < nums.length - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
+                    continue;
+                }
+                int L = j + 1;
+                int R = nums.length - 1;
+                while (L < R) {
+                    if (nums[i] + nums[j] + nums[L] + nums[R] == target) {
+                        List<Integer> result = new LinkedList<>();
+                        result.add(nums[i]);
+                        result.add(nums[j]);
+                        result.add(nums[L]);
+                        result.add(nums[R]);
+                        results.add(result);
+                        while (L < R && nums[L] == nums[L + 1]) {
+                            L++;
+                        }
+                        while (L < R && nums[R] == nums[R - 1]) {
+                            R--;
+                        }
+                        L++;
+                        R--;
+                    } else if (nums[i] + nums[j] + nums[L] + nums[R] < target) {
+                        L++;
+                    } else {
+                        R--;
+                    }
+                }
+            }
+        }
+        return results;
+    }
+
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.fourSum(new int[]{1, 0, -1, 0, -2, 2}, 0));
+        System.out.println(solution.fourSumByHashTable(new int[]{1, 0, -1, 0, -2, 2}, 0));
     }
 }
