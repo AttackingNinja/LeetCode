bc9083fabd1ce3046361b8462764fc4f6185afc6
Lee_Cooper <495203870@qq.com>
2020-03-03 12:02:00 +0800

    Divide&Conquer02

--- src/LeetCode240/Solution.java
+++ src/LeetCode240/Solution.java
@@ -31,7 +31,7 @@ public class Solution {
         return result1 || result2 || result3 || result4;
     }
 
-    public boolean searchMatrix(int[][] matrix, int target) {
+    public boolean searchMatrixByDC(int[][] matrix, int target) {
         if (matrix.length != 0 && matrix[0].length != 0) {
             int i = 0;
             int j = 0;
@@ -52,8 +52,29 @@ public class Solution {
         return false;
     }
 
+    public boolean searchMatrixByBinarySearch(int[][] matrix, int target) {
+        for (int i = 0; i < matrix.length; i++) {
+            int left = 0;
+            int right = matrix[0].length - 1;
+            if (left <= right && matrix[i][left] > target) {
+                return false;
+            }
+            while (left <= right) {
+                int mid = (left + right) / 2;
+                if (matrix[i][mid] == target) {
+                    return true;
+                } else if (matrix[i][mid] < target) {
+                    left = mid + 1;
+                } else if (matrix[i][mid] > target) {
+                    right = mid - 1;
+                }
+            }
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.searchMatrix(new int[][]{{-1, 3}}, 3));
+        System.out.println(solution.searchMatrixByDC(new int[][]{{-1, 3}}, 3));
     }
 }
